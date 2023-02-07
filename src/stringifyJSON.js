6114f21576bdc861273db87b5dbb1e5868c17b04
Brandon Woods <woods.brandon.m@gmail.com>
2023-02-06 17:32:17 -0800

    Fix stringifyJSON test cases and stringifyJSON functionality for numbers, strings, and booleans. began objects.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,64 @@
 
 // but you don't so you're going to write it from scratch:
 
+
+// I - object, array, values
+// O - string
+// C -
+// E - undefined and functions omitted in objects and array
+
+// check data type of input
+// recursive calls for objects and array
+// if object or array, check values for undefined or function
+// numbers, stings, and booleans are turned to string ('"string"')
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  var result = '';
+  // if undefined or function
+  if (undefined || typeof obj === 'function') {
+    return;
+  }
+  // if number
+  // number.toString();
+  if (typeof obj === 'number') {
+    result.concat(obj.toString());
+  }
+  // if string
+  // return
+  if (typeof obj === 'string') {
+    result.concat('\'' + obj + '\'');
+  }
+  // if boolean
+  // boolean.toString();
+  if (typeof obj === 'boolean') {
+    result.concat(obj.toString());
+  }
+  // object
+  // if object
+  // recursive call
+  if (typeof obj === 'object') {
+    // use object.keys make array of keys
+    // use length method on array to set last property to variable lastKey
+
+    result.concat('{');
+    // iterate over object keys
+    // pass each property in to recursive call
+    for (var i in obj) {
+      result.concat(stringifyJSON(i));
+      result.concat(':');
+      result.concat(stringifyJSON(obj[i]));
+      // if key equals lastKey
+      // check if final key equal to lastKey variable
+
+      result.concat(',');
+    }
+    result.concat('}');
+
+  }
+  // array
+  // if array
+    // recursive call
+
+  return result;
+
 };
