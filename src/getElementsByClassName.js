7f2a70f52a9a97158d00ede111b483858dc65ed7
Brandon Woods <woods.brandon.m@gmail.com>
2023-02-06 17:29:14 -0800

    clean up notes and psuedocode

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -9,8 +9,6 @@
 // O - array of elements that have the given className
 
 // recursion check - if it's a parent element in the DOM tree, perform function again
-
-
 var getElementsByClassName = function(className, element) {
 // define empty array
   var result = [];
@@ -20,29 +18,19 @@ var getElementsByClassName = function(className, element) {
   } else {
     parentElement = element;
   }
-
+  // base case
+  if (_.contains(parentElement.classList, className)) {
+    result.push(parentElement);
+  }
   // recurse case
   // iterate over the document
   // if current element is a parent node
-  if (_.contains(parentElement.classList, className)){
-    result.push(parentElement);
-  }
-
   if (parentElement.childNodes) {
     parentElement.childNodes.forEach(function (child) {
       // concat result array with result of recursed call function, passing in parent element (may need className again)
       result = result.concat(getElementsByClassName(className, child));
-    })
+    });
   }
-
-  // else {
-  //     // base case
-  //     // element is not parent
-  //     // push the element to empty array
-  //   if (parentElement.classList.contains(className)){
-  //       result.push(parentElement);
-  //   }
-  // }
   // return result array
   return result;
 };
